001071943b8882889df92d8d070ce99238f84c57
Ndelforno <nicolas.delforno@gmail.com>
2019-01-23 15:08:22 -0500

    addNewTask function added

--- src/components/tasksContainer.js
+++ src/components/tasksContainer.js
@@ -20,9 +20,28 @@ class TasksContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewTask = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/tasks',
+      {task:
+        {
+          title:'',
+          body:''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
+        <button className="newTaskButton" onClick={this.addNewTask}>
+          New Task
+        </button>
         {this.state.tasks.map((task) => {
           return(
             <Task task={task} key={task.id}/>
